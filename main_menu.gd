3c02d4e5718721f810a9fe042e13fe982d2d48cb
TheAbsentTourist <118998365+TheAbsentTourist@users.noreply.github.com>
2026-05-24 14:40:47 -0400

    Main menu prototype

---
+++ main_menu.gd
@@ -0,0 +1,11 @@
+extends Control
+
+
+
+func _on_arcade_button_pressed() -> void:
+	var scene_path = "uid://da8nnse7hu2wu"
+	get_tree().change_scene_to_file(scene_path)
+
+
+func _on_classic_button_pressed() -> void:
+	pass # Replace with function body.
