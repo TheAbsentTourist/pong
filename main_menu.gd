88b30c4fcc3135975afbd02a9f4e98aa1d017363
TheAbsentTourist <118998365+TheAbsentTourist@users.noreply.github.com>
2026-05-25 09:41:15 -0400

    Finalized main menu and added permament slider changing for mouse sensitivity

--- main_menu.gd
+++ main_menu.gd
@@ -7,10 +7,6 @@ func _on_arcade_button_pressed() -> void:
 	get_tree().change_scene_to_file(scene_path)
 
 
-func _on_classic_button_pressed() -> void:
-	pass # Replace with function body.
-
-
 func _on_exit_pressed() -> void:
 	get_tree().quit()
 
--- options.gd
+++ options.gd
@@ -5,11 +5,14 @@ extends Control
 @onready var sens_label: Label = $Sens_Slide/Sens_Label
 
 const SAVE_PATH = "user://settings.cfg"
-
 var current_sensitivity: float = 5.0
 
 func _ready() -> void:
 	sens_label.text = "CURRENT SENSITIVITY: " + "%.2f" % current_sensitivity
+	load_settings()
+	sens_slider.value = current_sensitivity
+	sens_label.text = "CURRENT SENSITIVITY: " + "%.2f" % current_sensitivity
+
 
 func _on_h_slider_value_changed(new_mouse_sensitivity: float) -> void:
 	current_sensitivity = new_mouse_sensitivity
@@ -37,3 +40,15 @@ func save_settings():
 func _on_reset_pressed() -> void:
 	# 2. Update the actual slider UI value
 	sens_slider.value = 5.0
+	
+
+func load_settings():
+	var config = ConfigFile.new()
+	var error = config.load(SAVE_PATH)
+	
+	# If the file loads successfully, grab the value. 
+	# If it doesn't exist yet (first time opening the game), it will default to 5.0
+	if error == OK:
+		current_sensitivity = config.get_value("Controls", "mouse_sensitivity", 5.0)
+	else:
+		print("No save file found or failed to load. Using default settings.")
